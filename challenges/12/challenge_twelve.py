7d47ec8bc7fbce0d4423166feb69336fdbce8b28
Simon Porter <simonporter007@users.noreply.github.com>
2018-08-17 17:45:03 +0100

    Added solution 12 and files, had to Google this one for serious hints

--- challenges/12/challenge_twelve.py
+++ challenges/12/challenge_twelve.py
@@ -1,9 +1,12 @@
-import gfx
-
+from imghdr import what
+from os import rename
 # View source for hints and follow pages to see images
 # Download hidden gfx file
-doc = gfx.open("pdf", "evil2.gfx")
-for pagenr in range(1,doc.pages+1):
-    page = doc.getPage(pagenr)
-    print ("Page", pagenr, "has dimensions", page.width, "x", page.height )
-
+data = open("evil2.gfx", 'rb').read()
+# The dealer is dealing into 5 piles, do the same with the data
+for i in range(5):
+    with open('%d' % i, 'wb') as f:
+        f.write(data[i::5])
+    # Find the type of file we have and rename it
+    ext = what('%d' % i)
+    rename('%d' % i, '%d.%s' % (i, ext))
