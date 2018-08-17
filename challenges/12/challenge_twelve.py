aed39bdc058869f32ed23fb2f45c2af0c8a252fd
Simon Porter <simonporter007@users.noreply.github.com>
2018-08-17 18:25:11 +0100

    Missed some clues from this challenge

--- challenges/12/challenge_twelve.py
+++ challenges/12/challenge_twelve.py
@@ -1,6 +1,18 @@
+from magic import from_file
+from base64 import b64decode
 from imghdr import what
 from os import rename
 # View source for hints and follow pages to see images
+# Last image does not display correctly, download for analysis
+if not what("evil4.jpg"):
+    # If not an image, what is it?
+    print(from_file("evil4.jpg"))
+
+# Reading the file looks familiar, lets get our clue
+# Clue doesn't help us! Note it for later and move on.
+coded_string = open("evil4.jpg", "rb").read()
+print(b64decode(coded_string))
+
 # Download hidden gfx file
 data = open("evil2.gfx", 'rb').read()
 # The dealer is dealing into 5 piles, do the same with the data
